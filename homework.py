a84d2a8f2e4c284bc9c1b3e9417915a559d500a8
Adam Gorske <adamgorske@gmail.com>
2022-06-02 15:34:54 -0700

    added an update note

--- homework.py
+++ homework.py
@@ -2,3 +2,5 @@
 # Write a function to print "hello_USERNAME!" USERNAME is the input of the function. The first line of the code has been defined as below.
 
     def hello_name(user_name):
+
+#update this and see what it does. 
